8d5a75d988ec4049d3db15baeb04c85f5c780ccd
mika76200 <mydiv@outlook.fr>
2021-09-17 13:43:15 +0200

    create

---
+++ README.md
@@ -0,0 +1,1 @@
+# MyDiv.js
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" >
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+
+  <footer>
+    <script type="text/javascript" src="MyDiv/MyDiv.js"></script>
+  </footer>
+
+  </script>
+</html>
